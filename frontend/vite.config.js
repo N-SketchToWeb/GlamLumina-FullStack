da742b3e7746abe8da3173f9a01eb556a6c0aacc
unknown <nanditanilajagi@gmail.com>
2026-01-17 00:37:04 +0530

    Fix Vite base path and update frontend config for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/create-order': 'http://localhost:8000', // Proxy requests to the backend
-//     },
-//   },
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GlamLumina/", // ✅ Only needed for GitHub Pages
+  base: '/', // ✅ Only needed for GitHub Pages
   plugins: [react()],
 });
-
-
